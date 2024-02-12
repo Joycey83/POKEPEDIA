7332f30e604372188dab4e45fcc511d7a45875af
Joyce <Jcheung8385@gmail.com>
2024-02-12 17:06:35 +0000

    I created a few an img div and h3 to test out if the data fetch from the pokeAPI will pull through

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,5 +1,19 @@
 function displayPokemon(response) {
   console.log(response);
+
+  const headingThree = document.createElement("h3");
+  headingThree.innerText = response.data.species.name;
+  document.body.appendChild(headingThree);
+
+  const imgDiv = document.createElement("img");
+  imgDiv.classList.add("pokemon-img");
+  // imgDiv.src = response.data.sprites.other.dream_world.front_default;
+  imgDiv.src = response.data.sprites.other.home.front_default;
+
+  // if (imgDiv.src === null) {
+  //   response.data.sprites.other.home.front_default;
+  // }
+  document.body.appendChild(imgDiv);
 }
-let url = `https://pokeapi.co/api/v2/pokemon/123`;
+let url = `https://pokeapi.co/api/v2/pokemon/1025`;
 axios.get(url).then(displayPokemon);
