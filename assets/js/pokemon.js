9750ec3fa7c9cbb15944ead49d8eb85199e4edd5
Joyce <Jcheung8385@gmail.com>
2024-02-13 14:18:16 +0000

    created another function to display more pokemon information

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -3,7 +3,7 @@ function displayPokemon(response) {
   const pokeContainer = document.querySelector(".pokemon-container");
   const pokeImgContainer = document.querySelector(".img-wrapper");
   const headingOne = document.querySelector(".poke-name");
-  headingOne.innerText = response.data.species.name;
+  headingOne.innerText = response.data.name;
   pokeContainer.appendChild(headingOne);
 
   // Getting both types
@@ -18,6 +18,27 @@ function displayPokemon(response) {
   imgDiv.src = response.data.sprites.other.home.front_default;
 
   pokeImgContainer.appendChild(imgDiv);
+
+  // Fetch more info
+  let url = `https://pokeapi.co/api/v2/pokemon-species/${response.data.id}/`;
+  axios.get(url).then(displayMoreInfo);
 }
-let url = `https://pokeapi.co/api/v2/pokemon/67`;
+
+let url = "https://pokeapi.co/api/v2/pokemon/54";
 axios.get(url).then(displayPokemon);
+
+function sanitizeString(str) {
+  return str.replace(/[^0-9a-z\s]/gi, "");
+}
+
+function displayMoreInfo(response) {
+  console.log(response);
+  const pokeContainer = document.querySelector(".poke--info");
+  const description = document.createElement("span");
+  description.innerText = sanitizeString(
+    response.data.flavor_text_entries[7].flavor_text
+  );
+  pokeContainer.appendChild(description);
+
+  console.log(response.data.flavor_text_entries[7].flavor_text);
+}
