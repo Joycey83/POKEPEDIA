016f645f5ea04ddb9284f67983ae537e6cc580c1
Joyce <Jcheung8385@gmail.com>
2024-02-13 13:18:09 +0000

    Testing out how to fetch the pokemon types array

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,19 +1,23 @@
 function displayPokemon(response) {
   console.log(response);
   const pokeContainer = document.querySelector(".pokemon-container");
-  const headingThree = document.createElement("h3");
-  headingThree.innerText = response.data.species.name;
-  document.body.appendChild(headingThree);
+  const pokeImgContainer = document.querySelector(".img-wrapper");
+  const headingOne = document.querySelector(".poke-name");
+  headingOne.innerText = response.data.species.name;
+  pokeContainer.appendChild(headingOne);
+
+  // Getting both types
+  response.data.types.forEach((type) => {
+    const typeName = document.createElement("p");
+    typeName.innerText = type.type.name;
+    pokeContainer.appendChild(typeName);
+  });
 
   const imgDiv = document.createElement("img");
-  imgDiv.classList.add("pokemon-img");
-  // imgDiv.src = response.data.sprites.other.dream_world.front_default;
+  imgDiv.classList.add("poke--img");
   imgDiv.src = response.data.sprites.other.home.front_default;
 
-  // if (imgDiv.src === null) {
-  //   response.data.sprites.other.home.front_default;
-  // }
-  pokeContainer.appendChild(imgDiv);
+  pokeImgContainer.appendChild(imgDiv);
 }
-let url = `https://pokeapi.co/api/v2/pokemon/255`;
+let url = `https://pokeapi.co/api/v2/pokemon/67`;
 axios.get(url).then(displayPokemon);
