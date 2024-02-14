861305f1666245a7858e0b6ea1500523f0b4011e
Joyce <Jcheung8385@gmail.com>
2024-02-14 14:23:55 +0000

    added my all my elements that I need to generate my data on to the Dom

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,4 +1,12 @@
-const MAX_POKEMON = 320;
+const MAX_POKEMON = 321;
+
+const inputElement = document.querySelector("#pokemon-name-input");
+const searchBtn = document.querySelector("#search-btn");
+const shuffleBtn = document.querySelector("#shuffle-btn");
+const pokemonContainer = document.querySelector(".poke-container");
+const pokemonNum = document.querySelector(".poke-number");
+const pokemonName = document.querySelector(".poke-name");
+const pokemonImg = document.querySelector(".poke-img");
 
 axios
   .get(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
@@ -6,3 +14,5 @@ axios
     allPokemons = response.data.results;
     console.log(allPokemons);
   });
+
+// Display my pokemon
