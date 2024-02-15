aff9aaf2408c2b609987d3585b97eb6b3802ce3e
Joyce <Jcheung8385@gmail.com>
2024-02-15 13:58:57 +0000

    created the displaypokemon function and added all the data that I want to fetch from the API

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -21,3 +21,61 @@ const catchThePokemon = async () => {
     console.log(error);
   }
 };
+
+const displayPokemon = (data) => {
+  console.log(data);
+  const hp = data.stats[0].base_stat;
+  const imgSrc = data.sprites.other.dream_world.front_default;
+  //   const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+  const pokeName = data.species.name.toUpperCase();
+  const statAttack = data.stats[1].base_stat;
+  const statDefense = data.stats[2].base_stat;
+  const statSpeed = data.stats[5].base_stat;
+
+  //   Create a new container for the type spans
+  const typesContainer = document.createElement("div");
+
+  // Getting both types and adding specific color type
+  data.types.forEach((type) => {
+    const typeName = document.createElement("span");
+    typeName.innerText = type.type.name;
+    typeName.classList.add(createColorSpan(type.type.name));
+    typesContainer.appendChild(typeName);
+  });
+
+  // Create elements
+  const nameElement = document.createElement("h1");
+  nameElement.className = "poke-name";
+  nameElement.textContent = pokeName;
+
+  const hpElement = document.createElement("p");
+  hpElement.className = "hp";
+  hpElement.innerHTML = `<span>HP</span> ${hp}`;
+
+  const imgElement = document.createElement("img");
+  imgElement.className = "poke-img";
+  imgElement.src = imgSrc;
+
+  const statsElement = document.createElement("div");
+  statsElement.className = "stats";
+  statsElement.innerHTML = `
+    <div>
+      <h3>${statAttack}</h3>
+      <p>Attack</p>
+    </div>
+    <div>
+      <h3>${statDefense}</h3>
+      <p>Defense</p>
+    </div>
+    <div>
+      <h3>${statSpeed}</h3>
+      <p>Speed</p>
+    </div>
+  `;
+
+  card.appendChild(nameElement);
+  card.appendChild(hpElement);
+  card.appendChild(imgElement);
+  card.appendChild(typesContainer);
+  card.appendChild(statsElement);
+};
