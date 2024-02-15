f2171a6003b9fbfbedf3d038621f600fbdfdf249
Joyce <Jcheung8385@gmail.com>
2024-02-15 13:56:30 +0000

    re-written my fetch API function as an Async function

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,25 +1,23 @@
 const inputElement = document.querySelector("#pokemon-name-input");
 const searchBtn = document.querySelector("#search-btn");
-const shuffleBtn = document.querySelector("#shuffle-btn");
+const shuffleBtn = document.querySelector("#shuffle-button");
 const pokemonContainer = document.querySelector(".poke-container");
 const pokemonNum = document.querySelector(".poke-number");
-const pokemonName = document.querySelector(".poke-name");
-const pokemonImg = document.querySelector(".poke-img");
+const pokemonCard = document.querySelector("#pokemon-card");
 
 const pokeApiUrl = " https://pokeapi.co/api/v2/pokemon/";
-// generate 321 Pokemon function
 
-function generatePokemon() {
-  const pokemonId = Math.floor(Math.random() * 350) + 1;
-
+const catchThePokemon = async () => {
+  card.innerHTML = "";
+  // Generate a random number between 1 and 150
+  let pokemonId = Math.floor(Math.random() * 350) + 1;
   // Combine the pokeapi url with pokemon id
   const finalUrl = pokeApiUrl + pokemonId;
-  axios
-    .get(finalUrl)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error("Error fetching data: ", error);
-    });
-}
+  // Fetch generated URL using axios
+  try {
+    const fetchPokemon = await axios.get(finalUrl);
+    displayPokemon(fetchPokemon.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
