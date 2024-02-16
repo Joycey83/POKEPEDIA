1b46382fe7f7f36603b7ebd723e33c74cff084a2
Joyce <Jcheung8385@gmail.com>
2024-02-16 14:22:31 +0000

    I had problems rendering my error image on the screen. I had to change to the correct file path

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -32,7 +32,7 @@ const catchThePokemon = async () => {
     const fetchPokemon = await axios.get(finalUrl);
     displayPokemon(fetchPokemon.data);
   } catch (error) {
-    errorMessage.innerHTML = `<h2 class="error-heading">OH NOOOOOOOOOOOOO invalid Pokemon Number or Name! Please try again</h2> <img class="error-img" src="../image/no-pokemon-img.png"/>`;
+    errorMessage.innerHTML = `<h2 class="error-heading">OH NOOOOOOOOOOOOO invalid Pokemon Number or Name! Please try again</h2> <img class="error-img" src="assets/image/no-pokemon-img.png"/>`;
   }
 };
 
